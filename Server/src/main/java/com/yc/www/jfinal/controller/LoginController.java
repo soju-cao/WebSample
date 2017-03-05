d7df4eb717319f96961972a4cab674a01202686d
Nick <1181476441@qq.com>
2017-03-05 23:41:22 +0800

    m

--- Server/src/main/java/com/yc/www/jfinal/controller/LoginController.java
+++ Server/src/main/java/com/yc/www/jfinal/controller/LoginController.java
@@ -14,7 +14,6 @@ public class LoginController extends Controller{
     public void login() {
        User user =  userService.findUser("nick", "nick");
         System.out.println(user);
-        //
         render("/html/hello.html");
     }
 }
